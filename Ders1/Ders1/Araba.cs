ab3cd8788779de921f076e2dd5c5d0416eaf5162
unknown <ayazibrahim13@gmail.com>
2025-03-06 15:03:37 +0300

    Hafta02

---
+++ Ders1/Ders1/Araba.cs
@@ -0,0 +1,38 @@
+﻿namespace Ders1
+{
+    public class Araba
+    {
+        /// <summary>
+        /// Sınıflar (class) PascalCase yazım kuralına göre yazılmalıdır.
+        /// Alanlar(Fields) camelCase yazım kuralına göre yazılmalıdır.
+        /// Özellikler(Properties) PascalCase yazım kuralına göre yazılmalıdır.
+        /// Metotlar(Methods) PascalCase veya camelCase yazım kuralına göre yazılmalıdıır.
+        /// </summary>
+        public string ad; //Field
+        public string Ad { get; set; } //Özellik
+        public string Renk { get; set; }//Özellik
+        public int ModelYili { get; set; }//Özellik
+        public string Marka { get; set; }//Özellik
+        public string YakitTipi { get; set; }//Özellik
+
+        public Araba()
+        {
+            
+        }
+
+        public void gazaBas()
+        {
+            Console.WriteLine("Araba hızlanıyor");
+        }
+        public void farAc(bool acikMi)
+        {
+            if (acikMi) {
+                Console.WriteLine($"{Ad} {Marka} isimli arabanın farı açıldı");
+            }
+            else
+            {
+                Console.WriteLine($"{Ad} isimli arabanın farı kapatıldı");
+            }
+        }
+    }
+}
---
+++ Ders1/Ders1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Ders1;
+
+Araba araba1 = new();
+araba1.Ad = "Fiesta";
+araba1.Marka = "Ford";
+araba1.Renk = "Beyaz";
+araba1.ModelYili = 2015;
+araba1.YakitTipi = "Benzin + LPG";
+
+araba1.gazaBas();
+
+araba1.farAc(true);
+
+Araba a2 = new();
+a2.Ad = "Toros";
+a2.Marka = "Renault";
+a2.Renk = "Beyaz";
+a2.ModelYili = 1998;
+a2.YakitTipi = "Benzin + LPG";
+
+a2.gazaBas();
+
+a2.farAc(true);
