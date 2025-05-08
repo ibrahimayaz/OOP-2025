5018c5d0ce79780c30b79632a65b226571f91d36
ibrahimayaz <ayazibrahim13@gmail.com>
2025-05-08 14:48:28 +0300

    Hafta 11

---
+++ Ders11/Kapsulleme/Kapsulleme/Program.cs
@@ -0,0 +1,5 @@
+﻿using Kapsulleme;
+
+Uye u = new("Ahmet", "NARİN", 20);
+
+Console.WriteLine(u.TamAd);
---
+++ Ders11/Kapsulleme/Kapsulleme/Uye.cs
@@ -0,0 +1,32 @@
+﻿namespace Kapsulleme
+{
+    public class Uye
+    {
+        private string ad;
+        private string soyAd;
+        private int yas;
+
+        public Uye(string ad, string soyAd, int yas)
+        {
+
+            this.ad = ad;
+            this.soyAd = soyAd;
+            this.yas = yas;
+        }
+        public int Yas
+        {
+            get { return yas; }
+            set
+            {
+                this.yas = value;
+            }
+        }
+        public string TamAd
+        {
+            get
+            {
+                return $"{ad} {soyAd}";
+            }
+        }
+    }
+}
