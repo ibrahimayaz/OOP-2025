54234e869b0365a74b1da0828903c042c9df873b
ibrahimayaz <ayazibrahim13@gmail.com>
2025-05-29 14:45:05 +0300

    Hafta14:Overloading

---
+++ Ders14/Overloading/Overloading/Matematik.cs
@@ -0,0 +1,23 @@
+﻿namespace Overloading
+{
+    public class Matematik
+    {
+        public int Topla(int x, int y)
+        {
+            return x + y;
+        }
+        public double Topla(int x, double y)
+        {
+            return x + y;
+        }
+        public double Topla(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Topla(double x, double y, double z)
+        {
+            return x + y + z;
+        }
+    }
+}
---
+++ Ders14/Overloading/Overloading/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Overloading;
+
+Matematik m1 = new();
+
+Console.WriteLine(m1.Topla(5.5, 6));
